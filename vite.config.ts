20cc0ca62ec8f602783218a9aebe07915efd7db3
Atila Erkek <atilaerk@gmail.com>
2026-04-26 14:37:02 +0300

    Update path alias in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/juri-puanlama-sistemi/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
 })
