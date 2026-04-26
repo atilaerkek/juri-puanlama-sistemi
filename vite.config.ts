21bd4711e1dbb5e924321837bdc278d5f6977b63
Atila Erkek <atilaerk@gmail.com>
2026-04-26 14:38:32 +0300

    Modify Vite config for path alias resolution
    Updated alias resolution for '@' to use path.resolve.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages için gerekli ana dizin ayarı
   base: '/juri-puanlama-sistemi/',
   resolve: {
     alias: {
-      "@": "/src",
+      // Projenin @ simgesini src klasörü olarak görmesini sağlar
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
